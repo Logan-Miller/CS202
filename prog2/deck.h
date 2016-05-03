2538dd20ea5500c0cefd7ac44aaa6d7f0b128abd
logan-miller <logan.h.miller@gmail.com>
2016-05-03 15:20:20 -0700

    Started S node class for the solitaire class. Going to need to make a copy constructor for the card class.

--- prog2/deck.h
+++ prog2/deck.h
@@ -3,18 +3,79 @@
 #include "card.h"
 
 /*
-    @desc:
+    @desc: A deck has an array of 52 cards. The constructor creates 52 unique
+           cards, ensuring 4 different suits each with 13 different values are 
+           created, and assigns each a position within the array.
 */
 class Deck
 {
 public:
     Deck();
-    ~Deck();
+    virtual ~Deck();
     void shuffle();
     void display_deck();
     void display_board();
 
 protected:
     Card ** the_deck;
-    Card *** the_board;
+//    Card *** the_board;
+};
+
+/*
+    @desc: S_nodes are used for creating doubly linked list type data 
+           structures of nodes containing a pointer to a card. 
+*/
+class S_node
+{
+public:
+    S_node();
+    ~S_node();
+    S_node * get_next();
+    S_node * get_prev();
+    S_node *& go_next();
+    S_node *& go_prev();
+    bool if_next() const;
+    bool if_prev() const;
+    void set_next(S_node * source);
+    void set_prev(S_node * source);
+    void display_node();
+protected:
+    S_node * next;
+    S_node * prev;
+    Card * card;
+};
+
+/*
+    @desc:
+*/
+class Solitaire : public Deck
+{
+public:
+    Solitaire();
+    virtual ~Solitaire();
+
+protected:
+
+};
+
+/*
+    @desc:
+*/
+class W_node
+{
+public:
+
+protected:
+
+};
+
+/*
+    @desc:
+*/
+class War
+{
+public:
+
+protected:
+
 };
